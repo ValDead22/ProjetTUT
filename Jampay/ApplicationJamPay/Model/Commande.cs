85c9284c0c60b77f942ad9a0bf1ce5b6e0d17129
Tom SANCHEZ <t.sanchez63@outlook.fr>
2018-01-22 11:24:42 +0100

    Creation classe Commande -- PlatCommandé

---
+++ Jampay/ApplicationJamPay/Model/Commande.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJamPay.Model
+{
+    class Commande
+    {
+        private int CodeCommande { get; set; }
+
+        private int idCaissier { get; set; }
+
+        private string MoyenDePaiement { get; set; }
+
+        private Caissier Caissier { get; set; }
+
+        private Carte Carte { get; set; }
+
+        public Commande(int CodeCommande, int idCaissier, string MoyenDePaiement)
+        {
+            this.CodeCommande = CodeCommande;
+            this.idCaissier = idCaissier;
+            this.MoyenDePaiement = MoyenDePaiement;
+        }
+    }
+}
