0b1c3aeee74dc43fccd4fffecd37d19d03b1f535
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-01-22 07:59:53 +0100

    Ajout dossier + Première classe model

---
+++ Jampay/ApplicationJamPay/Model/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJamPay.Model
+{
+    class Menu
+    {
+    }
+}
