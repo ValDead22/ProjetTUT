c4b02c452fea727f1f74ceead7bffb507e4f2851
Tom SANCHEZ <t.sanchez63@outlook.fr>
2018-01-22 10:53:44 +0100

    Modif Menu

--- Jampay/ApplicationJamPay/Model/Menu.cs
+++ Jampay/ApplicationJamPay/Model/Menu.cs
@@ -10,6 +10,8 @@ namespace ApplicationJamPay.Model
     {
         private int CodeMenu { get; set; }
 
+        private int idGerant { get; set; }
+
         private DateTime DateElaboration { get; set; }
 
         private string Categorie { get; set; }
@@ -20,7 +22,7 @@ namespace ApplicationJamPay.Model
 
         private Gerant Gerant { get; set; }
 
-        public Menu(int CodeMenu, DateTime DateElaboration, string Categorie, string Nom, string Observation)
+        public Menu(int CodeMenu,int idGerant, DateTime DateElaboration, string Categorie, string Nom, string Observation)
         {
             this.CodeMenu = CodeMenu;
             this.DateElaboration = DateElaboration;
