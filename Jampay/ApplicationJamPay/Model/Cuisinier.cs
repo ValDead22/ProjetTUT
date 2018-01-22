4a07c6f4e0f4492f280fa1a0b10c14f7a40b4f10
Tom SANCHEZ <t.sanchez63@outlook.fr>
2018-01-22 10:17:44 +0100

    Creation classe Cuisinier

---
+++ Jampay/ApplicationJamPay/Model/Cuisinier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJamPay.Model
+{
+    class Cuisinier : Utilisateur
+    {
+        private int idCuisinier { get; set; }
+
+        private string Fonction { get; set; }
+
+        private int idUtilisateur { get; set; }
+
+        public Cuisinier(int idCuisinier, string Fonction, int idUtilisateur) : base(Fonction,idUtilisateur)
+        {
+            this.idCuisinier = idCuisinier;
+        }
+    }
+}
