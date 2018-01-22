c37261d80a2e46226afadc5708d0faae4107b869
Tom SANCHEZ <t.sanchez63@outlook.fr>
2018-01-22 10:13:15 +0100

    Création classe Gerant

--- Jampay/ApplicationJamPay/Model/Gerant.cs
+++ Jampay/ApplicationJamPay/Model/Gerant.cs
@@ -6,20 +6,17 @@ using System.Threading.Tasks;
 
 namespace ApplicationJamPay.Model
 {
-    class Utilisateur : 
+    class Gerant : Utilisateur
     {
+        private int idGerant { get; set; }
+
         private string Fonction { get; set; }
 
         private int idUtilisateur { get; set; }
 
-        public Utilisateur(string Fonction, int idUtilisateur)
+        public Gerant(int idGerant, string Fonction, int idUtilisateur) : base(Fonction,idUtilisateur)
         {
-            this.Fonction = Fonction;
-            this.idUtilisateur = idUtilisateur;
-        }
-
-        public Utilisateur()
-        {
+            this.idGerant = idGerant;
         }
     }
 }
