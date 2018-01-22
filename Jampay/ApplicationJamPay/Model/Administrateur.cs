d9aac4cd4d0d52be5b920c7b3f2846a61c7b2f63
Tom SANCHEZ <t.sanchez63@outlook.fr>
2018-01-22 11:09:18 +0100

    Modif Administrateur et Usager

--- Jampay/ApplicationJamPay/Model/Administrateur.cs
+++ Jampay/ApplicationJamPay/Model/Administrateur.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 namespace ApplicationJamPay.Model
 {
-    class Administrateur : Utilisateur
+    class Administrateur : Usager
     {
+        private DateTime DateEntree { get; set; }
+
+        private DateTime DateFinContrat { get; set; }
+
+        private int Matricule { get; set; }
+
+        private int CodeFonction { get; set; }
+
+        private int Service { get; set; }
+
+        private string Nom { get; set; }
+
+        private string Prenom { get; set; }
+
+        private string Titre { get; set; }
+
         private int idAdmin { get; set; }
 
-        private string Fonction;
-
-        private int idUtilisateur;
-
-        public Administrateur(int idAdmin, string Fonction, int idUtilisateur) : base(Fonction, idUtilisateur)
+        public Administrateur(DateTime DateEntree,DateTime DateFinContrat, int Matricule, int CodeFonction, int Service, string Nom, string Prenom, string Titre, int idAdmin) : base(DateEntree,DateFinContrat,Matricule,CodeFonction,Service,Nom,Prenom,Titre)
         {
             this.idAdmin = idAdmin;
         }
