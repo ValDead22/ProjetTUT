8fd64f0e943161d71d361813a3079286cdd12f65
Tom SANCHEZ <t.sanchez63@outlook.fr>
2018-01-22 09:11:59 +0100

    Modification classe menu

--- Jampay/ApplicationJamPay/Model/Menu.cs
+++ Jampay/ApplicationJamPay/Model/Menu.cs
@@ -8,5 +8,29 @@ namespace ApplicationJamPay.Model
 {
     class Menu
     {
+        private int CodeMenu { get; set; }
+
+        private DateTime DateElaboration { get; set; }
+
+        private string Categorie { get; set; }
+
+        private string Nom { get; set; }
+
+        private string Observation { get; set; }
+
+        public Menu(int CodeMenu, DateTime DateElaboration, string Categorie, string Nom, string Observation)
+        {
+            this.CodeMenu = CodeMenu;
+            this.DateElaboration = DateElaboration;
+            this.Categorie = Categorie;
+            this.Nom = Nom;
+            this.Observation = Observation;
+        }
+
+
+
+         
+
+
     }
 }
