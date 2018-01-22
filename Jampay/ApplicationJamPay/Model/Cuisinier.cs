a81826cd44a9a9985388cf79daf2032c6f813169
Tom SANCHEZ <t.sanchez63@outlook.fr>
2018-01-22 11:12:57 +0100

    Modif Cuisinier

--- Jampay/ApplicationJamPay/Model/Cuisinier.cs
+++ Jampay/ApplicationJamPay/Model/Cuisinier.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 namespace ApplicationJamPay.Model
 {
-    class Cuisinier : Utilisateur
+    class Cuisinier : Usager
     {
+        private DateTime DateEntree { get; set; }
+
+        private DateTime DateFinContrat { get; set; }
+
+        private int Matricule { get; set; }
+
+        private int CodeFonction { get; set; }
+
+        private int Service { get; set; }
+
+        private string Nom { get; set; }
+
+        private string Prenom { get; set; }
+
+        private string Titre { get; set; }
+
         private int idCuisinier { get; set; }
 
-        private string Fonction { get; set; }
-
-        private int idUtilisateur { get; set; }
-
-        public Cuisinier(int idCuisinier, string Fonction, int idUtilisateur) : base(Fonction,idUtilisateur)
+        public Cuisinier(DateTime DateEntree, DateTime DateFinContrat, int Matricule, int CodeFonction, int Service, string Nom, string Prenom, string Titre, int idCuisinier) : base(DateEntree,DateFinContrat,Matricule,CodeFonction,Service,Nom,Prenom,Titre)
         {
             this.idCuisinier = idCuisinier;
         }
