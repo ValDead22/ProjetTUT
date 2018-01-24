b0e8cdc22ea44f692f741475dba347d3c44f6849
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-01-24 22:58:46 +0100

    Test de la connexion à la base de données

--- Jampay/ApplicationJamPay/View/MainWindow.xaml.cs
+++ Jampay/ApplicationJamPay/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationJamPay.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace ApplicationJamPay
         public MainWindow()
         {
             InitializeComponent();
+
+            ConnexionBD co =  new ConnexionBD();
         }
     }
 }
