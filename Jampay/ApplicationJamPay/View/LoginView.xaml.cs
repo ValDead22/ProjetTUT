5cb2fbef49961a732f74d7c06f6984f637207341
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-01-25 22:19:33 +0100

    Renommage + ajout binding (Erreur sur le mot de passe)

---
+++ Jampay/ApplicationJamPay/View/LoginView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ApplicationJamPay.Service;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ApplicationJamPay.View.Caissier;
+
+namespace ApplicationJamPay
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            Messenger.Default.Register<NotificationMessage>(this, NotificationMessageReceived);
+        }
+
+        private void NotificationMessageReceived(NotificationMessage msg)
+        {
+            if (msg.Notification == "OpenView")
+            {
+                var Caissier1 = new Caissier1();
+                Caissier1.Show();
+                this.Close();
+            }
+        }
+    }
+}
