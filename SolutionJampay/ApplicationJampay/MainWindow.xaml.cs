a1256b1abef13de62e9538f9f4be6ee6075fb167
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-01-28 18:17:54 +0100

    Ajout ViewModel + Changement nom propriétés binding

--- SolutionJampay/ApplicationJampay/MainWindow.xaml.cs
+++ SolutionJampay/ApplicationJampay/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ApplicationJampay.ViewModel.ViewModel;
 
 namespace ApplicationJampay
 {
@@ -23,6 +24,7 @@ namespace ApplicationJampay
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new LoginViewModel();
         }
     }
 }
