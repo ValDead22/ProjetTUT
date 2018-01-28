382413503f4afeadddc0e2b00aadeeb0cb4cd33b
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-01-28 18:18:33 +0100

    Dialog service pour accéder facilement aux fenêtres de l'application

---
+++ SolutionJampay/ApplicationJampay.Model/Service/DialogService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJampay.Model.Service
+{
+    public class DialogService
+    {
+        public static void ShowCaissierWindow()
+        {
+            var createType = Type.GetType("ApplicationJampay.View.Caissier1, ApplicationJampay");
+            var windows = (System.Windows.Window)Activator.CreateInstance(createType);
+
+            windows.ShowDialog();
+        }
+
+        public static void ShowGerantWindow()
+        {
+            var createType = Type.GetType("ApplicationJampay.View.Gérant1, ApplicationJampay");
+            var windows = (System.Windows.Window)Activator.CreateInstance(createType);
+
+            windows.ShowDialog();
+        }
+    }
+}
