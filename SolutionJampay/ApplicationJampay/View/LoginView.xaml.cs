4d62e6aea3da6fe0faa916f5817ad01e9915f3c8
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-01-29 07:30:52 +0100

    Chagement de la fenêtre de démarage

--- SolutionJampay/ApplicationJampay/View/LoginView.xaml.cs
+++ SolutionJampay/ApplicationJampay/View/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationJampay.ViewModel.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace ApplicationJampay.View
         public LoginView()
         {
             InitializeComponent();
+            DataContext = new LoginViewModel();
         }
     }
 }
