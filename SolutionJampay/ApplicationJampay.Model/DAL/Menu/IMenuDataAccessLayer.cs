3339830f1cb3a3ea12997c925e2f699a1a53b78c
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-01-29 09:12:15 +0100

    Ajout DALL pour les menus

---
+++ SolutionJampay/ApplicationJampay.Model/DAL/Menu/IMenuDataAccessLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJampay.Model.DAL.Menu
+{
+    interface IMenuDataAccessLayer
+    {
+        /// <summary>
+        /// Get all menus
+        /// </summary>
+        /// <returns></returns>
+        List<Entity.Menu> GetAllMenus();
+
+        /// <summary>
+        /// Add a new Menu
+        /// </summary>
+        /// <param name="menu">Menu to add</param>
+        void AddMenu(Entity.Menu menu);
+    }
+}
---
+++ SolutionJampay/ApplicationJampay.Model/DAL/Menu/MenuBusiness.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJampay.Model.DAL.Menu
+{
+    class MenuBusiness
+    {
+        private IMenuDataAccessLayer _menuDAL;
+
+        public MenuBusiness()
+        {
+            _menuDAL = new MenuDataAccessLayer();
+        }
+
+        public List<Entity.Menu> GetAllMenus()
+        {
+            return _menuDAL.GetAllMenus();
+        }
+
+
+        public void AddProduit(Entity.Menu menu)
+        {
+            _menuDAL.AddMenu(menu)
+        }
+    }
+}
