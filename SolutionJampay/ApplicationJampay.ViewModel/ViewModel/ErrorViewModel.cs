74ecaa8e1c8defd5fee730ce55a1bc8ccd604c35
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-04 14:54:52 +0100

    Ajout d'une vue d'erreur

---
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/ErrorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJampay.ViewModel.ViewModel
+{
+    public class ErrorViewModel : IViewModelBase
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+
+        public ErrorViewModel(string message)
+        {
+            _message = message;
+        }
+    }
+}
