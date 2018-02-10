1d2696c16905b7e8f4d0e6cbaa4ab93db0ded7e5
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-10 14:25:07 +0100

    ViewModel de la fenetre de config des menus du gérant

---
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/AjouterPlatGerantViewModel.cs
@@ -0,0 +1,46 @@
+﻿using ApplicationJampay.Model.DAL.Plat;
+using ApplicationJampay.Model.Entity;
+using ApplicationJampay.Model.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJampay.ViewModel.ViewModel
+{
+    public class AjouterPlatGerantViewModel : IViewModelBase
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private PlatBusiness _platBusiness;
+
+        private ObservableCollection<Plat> _collectionPlat = new ObservableCollection<Plat>();
+        public IEnumerable<Plat> CollectionPlats { get { return _collectionPlat; } }
+
+        public AjouterPlatGerantViewModel()
+        {
+            _platBusiness = new PlatBusiness();
+            
+
+            try
+            {
+                foreach (Plat plat in _platBusiness.GetAllPlat())
+                {
+                    _collectionPlat.Add(plat);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogService.ShowErrorWindow(ex.Message);
+            }
+        }
+    }
+}
