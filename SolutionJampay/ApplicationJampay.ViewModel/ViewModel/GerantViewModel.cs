59883876907413f55fc60ce7225c4a6abadac015
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-10 14:26:31 +0100

    Modif en accord avec le nouveau layout

--- SolutionJampay/ApplicationJampay.ViewModel/ViewModel/GerantViewModel.cs
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/GerantViewModel.cs
@@ -1,52 +1,71 @@
-﻿using System;
+﻿using ApplicationJampay.Model.DAL.Menu;
+using ApplicationJampay.Model.DAL.Plat;
+using ApplicationJampay.Model.Entity;
+using ApplicationJampay.Model.Service;
+using ApplicationJampay.ViewModel.Command;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Documents;
-using ApplicationJampay.Model.Entity;
-using ApplicationJampay.Model.DAL;
-using ApplicationJampay.Model.DAL.Menu;
-using ApplicationJampay.Model.Service;
-using ApplicationJampay.Model.DAL.Plat;
-using ApplicationJampay.ViewModel.Command;
 using System.Windows.Input;
 
 namespace ApplicationJampay.ViewModel.ViewModel
 {
+
+
     public class GerantViewModel : IViewModelBase
     {
+
+        #region Property stuff
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public Action Close;
 
         void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
 
+        private readonly RelayCommand _openModifyMenuWindow;
+        public ICommand OpenModifyMenuWindow => _openModifyMenuWindow;
 
+        /// <summary>
+        /// Logic to access to the Menus data
+        /// </summary>
         private MenuBusiness _menuBusiness;
+        /// <summary>
+        /// Logic to access to the PLats data
+        /// </summary>
         private PlatBusiness _platBusiness;
 
-        private readonly RelayCommand _selectionChanged;
-        public ICommand SelectionChanged => _selectionChanged;
+        #region ObservableCollections
 
         private ObservableCollection<Menu> _collectionMenu = new ObservableCollection<Menu>();
         public IEnumerable<Menu> CollectionMenus { get { return _collectionMenu; } }
-       
+
         private ObservableCollection<Plat> _collectionPlat = new ObservableCollection<Plat>();
         public IEnumerable<Plat> CollectionPlats { get { return _collectionPlat; } }
+
+        private ObservableCollection<Plat> _collectionPlatOfSelectedMenu = new ObservableCollection<Plat>();
+        public IEnumerable<Plat> CollectionPlatOfSelectedMenu { get { return _collectionPlatOfSelectedMenu; } }
+
+        #endregion
 
         public GerantViewModel()
         {
             _menuBusiness = new MenuBusiness();
             _platBusiness = new PlatBusiness();
 
-            _selectionChanged = new RelayCommand(() => UpdatePlatCollection(), o => true);
+            _openModifyMenuWindow = new RelayCommand(() => DialogService.ShowAjouterPlatGerantWindow(), o => true);
 
             try
             {
+                foreach (Plat plat in _platBusiness.GetAllPlat())
+                {
+                    _collectionPlat.Add(plat);
+
+                }
                 foreach (Menu menu in _menuBusiness.GetAllMenus())
                 {
                     _collectionMenu.Add(menu);
@@ -58,10 +77,13 @@ namespace ApplicationJampay.ViewModel.ViewModel
             }            
         }
 
+
+        
+
         private void UpdatePlatCollection()
         {
-            _collectionPlat.Clear();
-            _platBusiness.GetPlatByMenuId(SelectedMenu.CodeMenu).ForEach(p => _collectionPlat.Add(p));
+            _collectionPlatOfSelectedMenu.Clear();
+            _platBusiness.GetPlatByMenuId(SelectedMenu.CodeMenu).ForEach(p => _collectionPlatOfSelectedMenu.Add(p));
         }
 
         private Menu _selectedMenu;
