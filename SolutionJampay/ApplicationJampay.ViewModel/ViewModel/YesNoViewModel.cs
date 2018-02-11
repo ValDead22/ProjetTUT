95a7a48e1ffadc2e8a393efe63c064528be6c3af
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-11 23:28:29 +0100

    Viewmodel du dialog yes no

---
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/YesNoViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ApplicationJampay.ViewModel.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ApplicationJampay.ViewModel.ViewModel
+{
+    public class YesNoViewModel : IViewModelBase
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private readonly RelayCommand _yesCommand;
+        public ICommand YesCommand => _yesCommand;
+
+        private readonly RelayCommand _noCommand;
+        public ICommand NoCommand => _noCommand;
+
+        public Action Close;
+
+        
+
+        public YesNoViewModel(string msg, Action yes)
+        {
+            _yesCommand = new RelayCommand(() => { yes(); Close(); }, o => true);
+            _noCommand = new RelayCommand(() => Close(), o => true);
+            Msg = msg;
+        }
+
+        private string _msg;
+        public string Msg
+        {
+            get { return _msg; }
+            set
+            {
+                _msg = value;
+                OnPropertyChanged(nameof(Msg));
+            }
+        }
+
+    }
+}
