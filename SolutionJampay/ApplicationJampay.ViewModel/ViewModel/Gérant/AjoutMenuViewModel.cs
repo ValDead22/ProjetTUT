bf5521d862d1556f2035a79a9ff1624db36e1ac9
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-11 23:25:22 +0100

    View models pour les fenêtres d'ajout

---
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Gérant/AjoutMenuViewModel.cs
@@ -0,0 +1,117 @@
+﻿using ApplicationJampay.Model.DAL.Menu;
+using ApplicationJampay.ViewModel.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace ApplicationJampay.ViewModel.ViewModel.Gérant
+{
+    public class AjoutMenuViewModel : IViewModelBase
+    {
+        #region PropertyStuff
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+       
+        private MenuBusiness _menuBusiness;
+
+        public Action Close;
+
+        private readonly RelayCommand _createMenuCommand;
+        public ICommand CreateMenuCommand => _createMenuCommand;
+
+        public AjoutMenuViewModel()
+        {
+            _menuBusiness = new MenuBusiness();
+
+            _availableCategories = _menuBusiness.GetAllCategories();
+
+            _createMenuCommand = new RelayCommand(() => { CreateNewMenu(); Close(); }, o => true);
+
+        }
+
+
+        private void CreateNewMenu()
+        {
+            Model.Entity.Menu menu = new Model.Entity.Menu(_idGérant, _dateElaboration, _selectedCategory, _nom, _observation);
+
+            _menuBusiness.AddMenu(menu);
+        }
+
+
+        private string _nom;
+        public string Nom
+        {
+            get { return _nom; }
+            set
+            {
+                _nom = value;
+                OnPropertyChanged(nameof(Nom));
+            }
+        }
+
+        private int _idGérant;
+        public int IDGérant
+        {
+            get { return _idGérant; }
+            set
+            {
+                _idGérant = value;
+                OnPropertyChanged(nameof(IDGérant));
+            }
+        }
+
+        private DateTime _dateElaboration;
+        public DateTime DateElaboration
+        {
+            get { return _dateElaboration; }
+            set
+            {
+                _dateElaboration = value;
+                OnPropertyChanged(nameof(DateElaboration));
+            }
+        }
+
+        private string _observation;
+        public string Observation
+        {
+            get { return _observation; }
+            set
+            {
+                _observation = value;
+                OnPropertyChanged(nameof(Observation));
+            }
+        }
+
+
+        private List<string> _availableCategories;
+        public List<string> AvailableCategories
+        {
+            get { return _availableCategories; }
+            set
+            {
+                _availableCategories = value;
+                OnPropertyChanged(nameof(AvailableCategories));
+            }
+        }
+
+        private string _selectedCategory;
+        public string SelectedCategory
+        {
+            get { return _selectedCategory; }
+            set
+            {
+                _selectedCategory = value;
+                OnPropertyChanged(nameof(SelectedCategory));
+            }
+        }
+
+        
+    }
+}
---
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Gérant/AjoutPlatViewModel.cs
@@ -0,0 +1,127 @@
+﻿using ApplicationJampay.Model.DAL.Plat;
+using ApplicationJampay.Model.Entity;
+using ApplicationJampay.Model.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using GalaSoft.MvvmLight;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Messaging;
+using System.Diagnostics;
+using ApplicationJampay.ViewModel.Command;
+using System.Windows.Input;
+
+namespace ApplicationJampay.ViewModel.ViewModel.Gérant
+{
+    public class AjoutPlatViewModel : IViewModelBase
+    {
+        #region PropertyStuff
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private PlatBusiness _platBusiness;
+
+        public Action Close;
+
+        private readonly RelayCommand _createPlatCommand;
+        public ICommand CreatePlatCommand => _createPlatCommand;
+
+        public AjoutPlatViewModel()
+        {
+            _platBusiness = new PlatBusiness();
+
+            _availableCategories = _platBusiness.GetAllCategories();
+
+            _createPlatCommand = new RelayCommand(() => { CreateNewPlat(); Close(); }, o => true);
+            
+        }
+
+
+        private void CreateNewPlat()
+        {
+            Plat plat = new Plat(_tarif, _dateEffet, _dateFin, _selectedCategory, _nom);
+
+            _platBusiness.AddPlat(plat);
+        }
+
+
+        private string _nom;
+        public string Nom
+        {
+            get { return _nom; }
+            set
+            {
+                _nom = value;
+                OnPropertyChanged(nameof(Nom));
+            }
+        }
+
+        private DateTime _dateEffet;
+        public DateTime DateEffet
+        {
+            get { return _dateEffet; }
+            set
+            {
+                _dateEffet = value;
+                OnPropertyChanged(nameof(DateEffet));
+            }
+        }
+
+        private DateTime _dateFin;
+        public DateTime DateFin
+        {
+            get { return _dateFin; }
+            set
+            {
+                _dateFin = value;
+                OnPropertyChanged(nameof(DateFin));
+            }
+        }
+
+        private int _tarif;
+        public int Tarif
+        {
+            get { return _tarif; }
+            set
+            {
+                _tarif = value;
+                OnPropertyChanged(nameof(Tarif));
+            }
+        }
+
+
+        private List<string> _availableCategories;
+        public List<string> AvailableCategories
+        {
+            get { return _availableCategories; }
+            set
+            {
+                _availableCategories = value;
+                OnPropertyChanged(nameof(AvailableCategories));
+            }
+        }
+
+        private string _selectedCategory;
+        public string SelectedCategory
+        {
+            get { return _selectedCategory; }
+            set
+            {
+                _selectedCategory = value;
+                OnPropertyChanged(nameof(SelectedCategory));
+            }
+        }
+
+
+
+
+    }
+}
