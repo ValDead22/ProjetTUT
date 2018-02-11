7fbbcb1772d5db005fbec694f15185684eaf8fbc
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-11 23:26:59 +0100

    Renommage de la fenêtre principale du gérant

---
+++ SolutionJampay/ApplicationJampay/View/Gérant/GérantMainView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationJampay.ViewModel.ViewModel.Gérant;
+using System.Windows;
+
+namespace ApplicationJampay.View.Gérant
+{
+    /// <summary>
+    /// Logique d'interaction pour Gérant1.xaml
+    /// </summary>
+    public partial class GérantMainView : Window
+    {
+        public GérantMainView()
+        {
+            DataContext = new GérantMainViewModel()
+            {
+                Close = () => Close()
+            };
+
+            InitializeComponent();
+        }        
+    }
+}
