782cc90213cea0dd1d0decfaa1de843aae01e3c7
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-11 23:27:58 +0100

    Renommage

--- SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Gérant/GérantMainViewModel.cs
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Gérant/GérantMainViewModel.cs
@@ -3,23 +3,25 @@ using ApplicationJampay.Model.DAL.Plat;
 using ApplicationJampay.Model.Entity;
 using ApplicationJampay.Model.Service;
 using ApplicationJampay.ViewModel.Command;
+using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows.Input;
 
-namespace ApplicationJampay.ViewModel.ViewModel
+namespace ApplicationJampay.ViewModel.ViewModel.Gérant
 {
 
 
-    public class GerantViewModel : IViewModelBase
+    public class GérantMainViewModel : IViewModelBase
     {
 
         #region Property stuff
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public Action Close;
+        
 
         void OnPropertyChanged(string propertyName)
         {
@@ -27,8 +29,19 @@ namespace ApplicationJampay.ViewModel.ViewModel
         }
         #endregion
 
-        private readonly RelayCommand _openModifyMenuWindow;
-        public ICommand OpenModifyMenuWindow => _openModifyMenuWindow;
+        public Action Close;
+
+        private readonly RelayCommand _openAddingPlatWindow;
+        public ICommand OpenAddingPlatWindow => _openAddingPlatWindow;
+
+        private readonly RelayCommand _openAddingMenuWindow;
+        public ICommand OpenAddingMenuWindow => _openAddingMenuWindow;
+
+        private readonly RelayCommand _openDeletingMenuWindow;
+        public ICommand OpenDeletingMenuWindow => _openDeletingMenuWindow;
+
+        private readonly RelayCommand _openDeletingPlatWindow;
+        public ICommand OpenDeletingPlatWindow => _openDeletingPlatWindow;
 
         /// <summary>
         /// Logic to access to the Menus data
@@ -52,12 +65,16 @@ namespace ApplicationJampay.ViewModel.ViewModel
 
         #endregion
 
-        public GerantViewModel()
+        public GérantMainViewModel()
         {
             _menuBusiness = new MenuBusiness();
             _platBusiness = new PlatBusiness();
 
-            _openModifyMenuWindow = new RelayCommand(() => DialogService.ShowAjouterPlatGerantWindow(), o => true);
+            _openDeletingMenuWindow = new RelayCommand(() => DialogService.ShowYesNoWindow("Etes-vous sûr de vouloir supprimer ce menu ?", new Action(test1)), o => _selectedMenu != null);
+            _openDeletingPlatWindow = new RelayCommand(() => DialogService.ShowYesNoWindow("Etes-vous sûr de vouloir supprimer ce plat ?", new Action(test1)), o => _selectedPlat != null );
+
+            _openAddingPlatWindow = new RelayCommand(() => DialogService.ShowAjoutMenuView(), o => true);
+            _openAddingMenuWindow = new RelayCommand(() => DialogService.ShowAjoutPlatView(), o => true);
 
             try
             {
@@ -78,7 +95,19 @@ namespace ApplicationJampay.ViewModel.ViewModel
         }
 
 
-        
+        private void test1()
+        {
+            Debug.WriteLine("test yes");
+        }
+
+        private void test2()
+        {
+            Debug.WriteLine("test no");
+        }
+
+
+
+
 
         private void UpdatePlatCollection()
         {
@@ -96,8 +125,26 @@ namespace ApplicationJampay.ViewModel.ViewModel
             set
             {            
                 _selectedMenu = value;
+                _openDeletingMenuWindow.RaiseCanExecuteChanged();
                 OnPropertyChanged(nameof(SelectedMenu));
                 UpdatePlatCollection();
+            }
+        }
+
+
+
+        private Plat _selectedPlat;
+        public Plat SelectedPlat
+        {
+            get
+            {
+                return _selectedPlat;
+            }
+            set
+            {
+                _selectedPlat = value;
+                _openDeletingPlatWindow.RaiseCanExecuteChanged();
+                OnPropertyChanged(nameof(SelectedPlat));
             }
         }
     }
