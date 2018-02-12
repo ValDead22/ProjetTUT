8ebb60792ca584395a155fe775b1f0486a9612b3
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-12 10:28:01 +0100

    Aout de la section utilisateur

--- SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Gérant/GérantMainViewModel.cs
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Gérant/GérantMainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationJampay.Model.DAL.Menu;
 using ApplicationJampay.Model.DAL.Plat;
+using ApplicationJampay.Model.DAL.Utilisateur;
 using ApplicationJampay.Model.Entity;
 using ApplicationJampay.Model.Service;
 using ApplicationJampay.ViewModel.Command;
@@ -43,6 +44,12 @@ namespace ApplicationJampay.ViewModel.ViewModel.Gérant
         private readonly RelayCommand _openDeletingPlatWindow;
         public ICommand OpenDeletingPlatWindow => _openDeletingPlatWindow;
 
+        private readonly RelayCommand _openModifyingMenuWindow;
+        public ICommand OpenModifyingMenuWindow => _openModifyingMenuWindow;
+
+        private readonly RelayCommand _openModifyingPlatWindow;
+        public ICommand OpenModifyingPlatWindow => _openModifyingPlatWindow;
+
         /// <summary>
         /// Logic to access to the Menus data
         /// </summary>
@@ -51,6 +58,8 @@ namespace ApplicationJampay.ViewModel.ViewModel.Gérant
         /// Logic to access to the PLats data
         /// </summary>
         private PlatBusiness _platBusiness;
+
+        private UserBusiness _userBusiness;
 
         #region ObservableCollections
 
@@ -63,30 +72,42 @@ namespace ApplicationJampay.ViewModel.ViewModel.Gérant
         private ObservableCollection<Plat> _collectionPlatOfSelectedMenu = new ObservableCollection<Plat>();
         public IEnumerable<Plat> CollectionPlatOfSelectedMenu { get { return _collectionPlatOfSelectedMenu; } }
 
+        private ObservableCollection<Utilisateur> _collectionUtilisateur = new ObservableCollection<Utilisateur>();
+        public IEnumerable<Utilisateur> CollectionUtilisateur { get { return _collectionUtilisateur; } }
+
         #endregion
 
         public GérantMainViewModel()
         {
             _menuBusiness = new MenuBusiness();
             _platBusiness = new PlatBusiness();
+            _userBusiness = new UserBusiness();
+
 
             _openDeletingMenuWindow = new RelayCommand(() => DialogService.ShowYesNoWindow("Etes-vous sûr de vouloir supprimer ce menu ?", new Action(test1)), o => _selectedMenu != null);
-            _openDeletingPlatWindow = new RelayCommand(() => DialogService.ShowYesNoWindow("Etes-vous sûr de vouloir supprimer ce plat ?", new Action(test1)), o => _selectedPlat != null );
+            _openDeletingPlatWindow = new RelayCommand(() => DialogService.ShowYesNoWindow("Etes-vous sûr de vouloir supprimer ce plat ?", new Action(test1)), o => _selectedPlat != null);
 
             _openAddingPlatWindow = new RelayCommand(() => DialogService.ShowAjoutMenuView(), o => true);
             _openAddingMenuWindow = new RelayCommand(() => DialogService.ShowAjoutPlatView(), o => true);
+
+            _openModifyingMenuWindow = new RelayCommand(() => DialogService.ShowModifMenuView(), o => _selectedMenu == null);
+            _openModifyingPlatWindow = new RelayCommand(() => DialogService.ShowModifPlatView(), o => _selectedPlat != null);
 
             try
             {
                 foreach (Plat plat in _platBusiness.GetAllPlat())
                 {
                     _collectionPlat.Add(plat);
-
                 }
                 foreach (Menu menu in _menuBusiness.GetAllMenus())
                 {
                     _collectionMenu.Add(menu);
                 }
+                foreach (Utilisateur utilisateur in _userBusiness.GetAllUser())
+                {
+                    _collectionUtilisateur.Add(utilisateur);
+                }
+
             }
             catch (Exception ex)
             {
@@ -105,8 +126,24 @@ namespace ApplicationJampay.ViewModel.ViewModel.Gérant
             Debug.WriteLine("test no");
         }
 
-
-
+        private List<string> _availableFonction;
+        public List<string> AvailableFonction
+        {
+            get
+            {
+                return new List<string>
+                {
+                    "Gérant",
+                    "Caissier",
+                    "Administrateur"
+                };
+            }
+            set
+            {
+                _availableFonction = value;
+                OnPropertyChanged(nameof(AvailableFonction));
+            }
+        }
 
 
         private void UpdatePlatCollection()
@@ -130,8 +167,6 @@ namespace ApplicationJampay.ViewModel.ViewModel.Gérant
                 UpdatePlatCollection();
             }
         }
-
-
 
         private Plat _selectedPlat;
         public Plat SelectedPlat
