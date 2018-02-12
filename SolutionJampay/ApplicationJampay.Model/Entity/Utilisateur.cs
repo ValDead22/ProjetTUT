3e789bc3b4e6ce00723b923680b6cd21a9acba21
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-12 10:26:05 +0100

    Ajout d'un constructeur

--- SolutionJampay/ApplicationJampay.Model/Entity/Utilisateur.cs
+++ SolutionJampay/ApplicationJampay.Model/Entity/Utilisateur.cs
@@ -12,11 +12,23 @@ namespace ApplicationJampay.Model.Entity
 
         public int IdUtilisateur { get; private set; }
 
+        public string Nom { get; private set; }
+
+        public string Prenom { get; private set; }
+
         public Utilisateur(string Fonction, int idUtilisateur)
         {
             this.Fonction = Fonction;
             this.IdUtilisateur = idUtilisateur;
         }
-                
+
+        public Utilisateur(int idUtilisateur, string Fonction, string nom, string prenom)
+        {
+            this.Fonction = Fonction;
+            this.IdUtilisateur = idUtilisateur;
+            this.Nom = nom;
+            this.Prenom = prenom;
+        }
+
     }
 }
