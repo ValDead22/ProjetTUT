f653b7644299e6ef89d221e4104a55f1e630d8d6
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-21 18:59:49 +0100

    Convertisseur pour configurer l'activation d'un bouton ou non avec plusieurs valeurs

---
+++ SolutionJampay/ApplicationJampay/Converter/MultipleSelectedItemToIsEnable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ApplicationJampay.Converter
+{
+    class MultipleSelectedItemToIsEnable : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var cpt = 0;
+
+            foreach(object o in values)
+            {
+                if (o != null)
+                {
+                    cpt++;
+                }
+            }
+            
+            if (cpt == values.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
