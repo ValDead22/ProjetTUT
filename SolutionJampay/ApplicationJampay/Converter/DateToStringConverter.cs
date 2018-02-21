ee6bc3281fe32dbdcf30105d52a1d6343373587b
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-21 17:24:46 +0100

    Convertisseur pour afficher un format de date correct et pouvoir get une date depuis en champ de text

---
+++ SolutionJampay/ApplicationJampay/Converter/DateToStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ApplicationJampay.Converter
+{
+    class DateToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return String.Format("{0:dd/MM/yyyy}", value);
+            }
+            catch
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return DateTime.ParseExact(value as string, "dd/MM/yyyy", null);
+            }
+            catch
+            {
+                return value;
+            }
+        }
+    }
+}
