e04c95437d4968d8be7bdf7bb115df09ffee74fa
Valentin REVERSAT <valxbox22@live.fr>
2018-02-23 15:29:06 +0100

    test

--- SolutionJampay/ApplicationJampay.ViewModel/ViewModel/LoginViewModel.cs
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/LoginViewModel.cs
@@ -11,6 +11,7 @@ using ApplicationJampay.Model.Entity;
 using ApplicationJampay.Model.Service;
 using ApplicationJampay.ViewModel.Command;
 
+
 namespace ApplicationJampay.ViewModel.ViewModel
 {
     public class LoginViewModel : IViewModelBase
