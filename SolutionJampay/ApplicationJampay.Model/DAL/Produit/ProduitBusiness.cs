2ac4dd37fc39c535dad05d758cd3fe384814ec1c
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-25 22:16:54 +0100

    Renommage + get des produits par id du plat

---
+++ SolutionJampay/ApplicationJampay.Model/DAL/Produit/ProduitBusiness.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJampay.Model.DAL.Produit
+{
+    public class ProduitBusiness
+    {
+        private IProduitDataAccessLayer _productDAL;
+
+        public ProduitBusiness()
+        {
+            _productDAL = new ProduitDataAccessLayer();
+        }
+        
+        public List<Entity.Produit> GetAllProduits()
+        {
+            return _productDAL.GetAllProduits();
+        }
+
+        public List<Entity.Produit> GetProduitsByPlatId(int idPlat)
+        {            
+            try
+            {
+                return _productDAL.GetProduitsByPlatId(idPlat);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+        public void AddProduit(Entity.Produit produit)
+        {
+            _productDAL.AddProduit(produit);
+        }
+    }
+}
---
+++ SolutionJampay/ApplicationJampay.Model/DAL/Produit/ProduitDataAccessLayer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationJampay.Model.Entity;
+using ApplicationJampay.Model.Service;
+using MySql.Data.MySqlClient;
+
+namespace ApplicationJampay.Model.DAL.Produit
+{
+    class ProduitDataAccessLayer : IProduitDataAccessLayer
+    {
+        private SQLService _sQLService = SQLService.Instance;
+
+
+        public void AddProduit(Entity.Produit produit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entity.Produit> GetAllProduits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entity.Produit> GetProduitsByPlatId(int idPlat)
+        {
+            var query = "SELECT pr.CodeProduit, pr.DateEffet, pr.DateFin, pr.Categorie, pr.Nom, pr.Observation " +
+                "FROM Produit pr, Plat pl, CompositionPlat cpp " +
+                "WHERE pl.CodePlat =" + "\"" + idPlat + "\"" + "and pl.CodePlat = cpp.CodePlat and pr.CodeProduit = cpp.CodeProduit";
+            MySqlDataReader mySqlDataReader = _sQLService.Load(query);
+
+            try
+            {
+                List<Entity.Produit> list = new List<Entity.Produit>();
+                
+                while (mySqlDataReader.Read())
+                {
+                    Entity.Produit plat = new Entity.Produit(
+                        (int)mySqlDataReader["CodeProduit"],
+                        (DateTime)mySqlDataReader["DateEffet"],
+                        (DateTime)mySqlDataReader["DateFin"],
+                        mySqlDataReader["Categorie"] as string,
+                        mySqlDataReader["Nom"] as string,
+                        mySqlDataReader["Observation"] as string);
+
+                    list.Add(plat);
+                }
+                return list;
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Problème lors du chargement des plats !" + "\n" + ex.Message);
+            }
+            finally
+            {
+                mySqlDataReader.Close();
+            }
+        }
+    }
+}
