6fb129b04f3a696794779dd3193a90483a676410
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-25 21:14:38 +0100

    Création fenêtre cuisinier

---
+++ SolutionJampay/ApplicationJampay/View/Cuisinier/CuisinierMainView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ApplicationJampay.View.Cuisinier
+{
+    /// <summary>
+    /// Logique d'interaction pour CuisinierMainView.xaml
+    /// </summary>
+    public partial class CuisinierMainView : Window
+    {
+        public CuisinierMainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
