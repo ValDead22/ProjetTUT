df674c5dc327080f1836725953b9880feed21217
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-26 21:49:34 +0100

    Changment de package

--- SolutionJampay/ApplicationJampay/View/Gérant/PopUp/AjoutUtilisateurView.xaml.cs
+++ SolutionJampay/ApplicationJampay/View/Gérant/PopUp/AjoutUtilisateurView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace ApplicationJampay.View.Gérant
+namespace ApplicationJampay.View.Gérant.PopUp
 {
     /// <summary>
     /// Logique d'interaction pour AjoutUtilisateurView.xaml
