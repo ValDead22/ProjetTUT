4851afeed9bfbbebdf14a46267225e1c376196a2
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-26 23:40:37 +0100

    Nettoyage complet

--- SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Caissier/AjouterPlatViewModelcs.cs
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Caissier/AjouterPlatViewModelcs.cs
@@ -12,14 +12,15 @@ using ApplicationJampay.Model.Entity;
 using ApplicationJampay.Model.DAL;
 using ApplicationJampay.Model.DAL.Plat;
 using ApplicationJampay.Model.Service.Dialog;
+using GalaSoft.MvvmLight.Messaging;
 
 namespace ApplicationJampay.ViewModel.ViewModel.Caissier
 {
     public class AjouterPlatViewModel : IViewModelBase
     {
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        private PlatBusiness _platBusiness;
+        #region Property stuff
+        public event PropertyChangedEventHandler PropertyChanged;        
 
 
         void OnPropertyChanged(string propertyName)
@@ -27,17 +28,9 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        #endregion        
 
-        public AjouterPlatViewModel()
-        {
-            _platBusiness = new PlatBusiness();
-
-            _displayPlatCommand = new RelayCommand(() => Plats(), o => true);
-            _displayEntreeCommand = new RelayCommand(() => Entrees(), o => true);
-            _displayDessertCommand = new RelayCommand(() => Desserts(), o => true);
-            _displaySnackCommand = new RelayCommand(() => Snacks(), o => true);
-
-        }
+        #region Commands
         private readonly RelayCommand _displayPlatCommand;
         public ICommand DisplayPlatViewCommand => _displayPlatCommand;
 
@@ -50,16 +43,53 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier
         private readonly RelayCommand _displaySnackCommand;
         public ICommand DisplaySnackViewCommand => _displaySnackCommand;
 
+        private readonly RelayCommand _validateCommand;
+        public ICommand ValidateCommand => _validateCommand;
+        #endregion
 
+        private PlatBusiness _platBusiness;
+
+        public Action Close;
 
         private ObservableCollection<Plat> _coollectionOfSelectedCateg = new ObservableCollection<Plat>();
         public IEnumerable<Plat> CollectionOfSelectedCateg { get { return _coollectionOfSelectedCateg; } }
 
+        public AjouterPlatViewModel()
+        {
+            _platBusiness = new PlatBusiness();
+
+            _displayPlatCommand = new RelayCommand(() => Plats(), o => true);
+            _displayEntreeCommand = new RelayCommand(() => Entrees(), o => true);
+            _displayDessertCommand = new RelayCommand(() => Desserts(), o => true);
+            _displaySnackCommand = new RelayCommand(() => Snacks(), o => true);
+
+            _validateCommand = new RelayCommand(() => AddPLatToCommande(), o => true);
+
+        }
 
 
+        private Plat _selectedPlat;
+        public Plat SelectedPlat
+        {
+            get
+            {
+                return _selectedPlat;
+            }
+            set
+            {
+                _selectedPlat = value;
+                OnPropertyChanged(nameof(SelectedPlat));
+            }
+        }
 
+        private void AddPLatToCommande()
+        {
+            Messenger.Default.Send<Plat>(SelectedPlat);
+            Close();
 
-        public void Plats()
+        }
+
+        private void Plats()
         {
             _coollectionOfSelectedCateg.Clear();
 
@@ -67,63 +97,77 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier
             try
             {
                 listPlats = _platBusiness.GetPlatbyCateg("Plat");
+
+                foreach (Plat plat in listPlats)
+                {
+                    _coollectionOfSelectedCateg.Add(plat);
+                }
             }
             catch (Exception ex)
             {
-
                 DialogService.ShowErrorWindow(ex.Message);
             }
-
-            foreach(Plat plat in listPlats)
-            {
-                _coollectionOfSelectedCateg.Add(plat);
-            }
-
         }
 
-        public void Entrees()
-       {
+        private void Entrees()
+        {
             _coollectionOfSelectedCateg.Clear();
 
             List<Plat> listEntrees = new List<Plat>();
 
-            listEntrees = _platBusiness.GetPlatbyCateg("Entree");
-
-            foreach (Plat entree in listEntrees)
+            try
             {
-                _coollectionOfSelectedCateg.Add(entree);
+                listEntrees = _platBusiness.GetPlatbyCateg("Entree");
+
+                foreach (Plat entree in listEntrees)
+                {
+                    _coollectionOfSelectedCateg.Add(entree);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogService.ShowErrorWindow(ex.Message);
             }
         }
 
-        public void Desserts()
+        private void Desserts()
         {
-
             _coollectionOfSelectedCateg.Clear();
             List<Plat> listDesserts = new List<Plat>();
 
-            listDesserts = _platBusiness.GetPlatbyCateg("Dessert");
-
-            foreach (Plat dessert in listDesserts)
+            try
             {
-                _coollectionOfSelectedCateg.Add(dessert);
+                listDesserts = _platBusiness.GetPlatbyCateg("Dessert");
+
+                foreach (Plat dessert in listDesserts)
+                {
+                    _coollectionOfSelectedCateg.Add(dessert);
+                }
             }
-
-
-
+            catch (Exception ex)
+            {
+                DialogService.ShowErrorWindow(ex.Message);
+            }
         }
 
-        public void Snacks()
+        private void Snacks()
         {
-
             _coollectionOfSelectedCateg.Clear();
 
             List<Plat> listSnacks = new List<Plat>();
 
-            listSnacks = _platBusiness.GetPlatbyCateg("Snack");
-
-            foreach (Plat snack in listSnacks)
+            try
             {
-                _coollectionOfSelectedCateg.Add(snack);
+                listSnacks = _platBusiness.GetPlatbyCateg("Snack");
+
+                foreach (Plat snack in listSnacks)
+                {
+                    _coollectionOfSelectedCateg.Add(snack);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogService.ShowErrorWindow(ex.Message);
             }
 
         }
--- SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Caissier/CaissierViewModel.cs
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Caissier/CaissierViewModel.cs
@@ -3,10 +3,12 @@ using ApplicationJampay.Model.Entity;
 using ApplicationJampay.Model.Service;
 using ApplicationJampay.Model.Service.Dialog;
 using ApplicationJampay.ViewModel.Command;
+using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,30 +18,27 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier
 {
     public class CaissierViewModel : IViewModelBase
     {
+        #region Property Stuff
         public event PropertyChangedEventHandler PropertyChanged;
 
         void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        //List implémente IEnumerable, pas de sousses
+        } 
+        #endregion
+        
 
         private ObservableCollection<Plat> _collectionPlat = new ObservableCollection<Plat>();
         public IEnumerable<Plat> CollectionPlat { get { return _collectionPlat; } }
 
-
+        
         public CaissierViewModel()
         {
-
             _openAddPlatViewCommand = new RelayCommand(() => AddPlat(), o => true);
-           
 
-
+            Messenger.Default.Register<Plat>(this, HandleMessage);
         }
 
-        private Plat _platService;
-        //Table sélectionnée dans la ListView
         private Plat _selectedPlat;
         public Plat SelectedPlat
         {
@@ -51,58 +50,21 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier
             {
                 _selectedPlat = value;
                 OnPropertyChanged(nameof(SelectedPlat));
-                //Mettre à jour le canExecute de la fenêtre
-                _openModifyPlatViewCommand.RaiseCanExecuteChanged();
-                _openRemovePlatViewCommand.RaiseCanExecuteChanged();
             }
         }
 
-        //Table créée
-        private Plat _newPlat;
-        public Plat NewPlat
+        private void HandleMessage(Plat plat)
         {
-            get { return _newPlat; }
-            set { _newPlat = value; }
+            //Debug.WriteLine(plat.ToString());
         }
-
-
-        public RelayCommand AjoutP { get; private set; }
-        public RelayCommand Reglement { get; private set; }
 
         private readonly RelayCommand _openAddPlatViewCommand;
         public ICommand OpenAddPlatViewCommand => _openAddPlatViewCommand;
-
-        private readonly RelayCommand _openCreatePlatViewCommand;
-        public ICommand OpenCreatePlatViewCommand => _openCreatePlatViewCommand;
-
-        //Commande pour ouvrir la fenêtre de suppression
-        private readonly RelayCommand _openRemovePlatViewCommand;
-        public ICommand OpenRemoveTableViewCommand => _openRemovePlatViewCommand;
-
-        //Commande pour ouvrir la fenêtre de modification
-        private readonly RelayCommand _openModifyPlatViewCommand;
-        public ICommand OpenModifyTableViewCommand => _openModifyPlatViewCommand;
-
-        //Commande pour modifier une table
-        private readonly RelayCommand _modifyPlatComand;
-        public ICommand ModifyTableCommand => _modifyPlatComand;
-
-        //Commande pour supprimer une table
-        private readonly RelayCommand _deletePlatComand;
-        public ICommand DeleteTableCommand => _deletePlatComand;
-
-        //Commande pour annuler une suppression
-        private readonly RelayCommand _cancelDeletePlatComand;
-        public ICommand CancelDeleteTableCommand => _cancelDeletePlatComand;
-
-
-
+        
 
         public void AddPlat()
         {
-
             DialogCaissier.ShowAjouterPlatWindow();
-
         }
     }
 }
