96a29648e5c6c671c91e99654a529455e82ed74c
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-02-27 20:33:37 +0100

    Ajout d'un plat avec quantité pour l'afficher proprement dans la vue du caisqier sans casser tout le model

---
+++ SolutionJampay/ApplicationJampay.Model/Entity/PlatWithQuantité.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationJampay.Model.Entity
+{
+    public class PlatWithQuantité : Plat, INotifyPropertyChanged
+    {
+        #region PropertyChanged stuff
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        private int _quantite;
+        public int Quantite
+        {
+            get { return _quantite; }
+            set {
+                _quantite = value;
+                OnPropertyChanged(nameof(Quantite));
+            }
+        }
+
+        
+        public PlatWithQuantité(int codePlat, DateTime dateEffet, DateTime dateFin, string categorie, string nom, int quantite, float? prix = 0) : base(codePlat, dateEffet, dateFin, categorie, nom, prix)
+        {
+            Quantite = quantite;
+        }
+        
+    }
+}
