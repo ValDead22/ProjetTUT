0bd6b5dc17e3fef43f3f6a4e8fb78b413f22d492
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-03-04 22:47:40 +0100

    UserControl pour chaque action du caissier

---
+++ SolutionJampay/ApplicationJampay/View/Caissier/UserControlFolder/WelcomingUserControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationJampay.ViewModel.ViewModel.Caissier.UserControlFolder;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationJampay.View.Caissier.UserControlFolder
+{
+    /// <summary>
+    /// Logique d'interaction pour WelcomingUserControl.xaml
+    /// </summary>
+    public partial class WelcomingUserControl : UserControl
+    {
+        public WelcomingUserControl()
+        {
+            DataContext = new UserControlWelcomingViewModel();
+            InitializeComponent();
+        }
+    }
+}
