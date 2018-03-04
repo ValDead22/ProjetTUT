b1f85090e3c7f56470e3ba926a73033f93bd6336
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-03-04 22:49:43 +0100

    Ajout de getteur pour les nouveaux users controls

--- SolutionJampay/ApplicationJampay.Model/Service/Dialog/DialogCaissier.cs
+++ SolutionJampay/ApplicationJampay.Model/Service/Dialog/DialogCaissier.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ApplicationJampay.Model.Service.Dialog
 {
@@ -28,6 +29,39 @@ namespace ApplicationJampay.Model.Service.Dialog
             var windows = (System.Windows.Window)Activator.CreateInstance(createType);
 
             windows.ShowDialog();
+        }
+
+        /// <summary>
+        /// Show the caisse into the main view of the Caissier
+        /// </summary>
+        public static FrameworkElement GetCaisseUserControl()
+        {
+            var createType = Type.GetType("ApplicationJampay.View.Caissier.UserControlFolder.CaisseUserControl, ApplicationJampay");
+            var usercontrol = (System.Windows.Controls.UserControl)Activator.CreateInstance(createType);
+
+            return usercontrol;
+        }
+
+        /// <summary>
+        /// Show the the welcoming message into the main view of the Caissier
+        /// </summary>
+        public static FrameworkElement GetWelcomingUserControl()
+        {
+            var createType = Type.GetType("ApplicationJampay.View.Caissier.UserControlFolder.WelcomingUserControl, ApplicationJampay");
+            var usercontrol = (System.Windows.Controls.UserControl)Activator.CreateInstance(createType);
+
+            return usercontrol;
+        }
+
+        /// <summary>
+        /// Show the the facture manager into the main view of the Caissier
+        /// </summary>
+        public static FrameworkElement GetFactureUserControl()
+        {
+            var createType = Type.GetType("ApplicationJampay.View.Caissier.UserControlFolder.FactureUserControl, ApplicationJampay");
+            var usercontrol = (System.Windows.Controls.UserControl)Activator.CreateInstance(createType);
+
+            return usercontrol;
         }
     }
 }
