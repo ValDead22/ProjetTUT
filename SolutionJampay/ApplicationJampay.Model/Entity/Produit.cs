3b24ca0e5cc7bc0f1a262e8e8010a3ab53240c21
Valentin REVERSAT <valentin.reversat22@gmail.com>
2018-03-12 20:17:34 +0100

    Ajout d'un equals

--- SolutionJampay/ApplicationJampay.Model/Entity/Produit.cs
+++ SolutionJampay/ApplicationJampay.Model/Entity/Produit.cs
@@ -9,16 +9,12 @@ namespace ApplicationJampay.Model.Entity
     public class Produit
     {
         public int CodeProduit { get; private set; }
-
         public DateTime DateEffet { get; private set; }
-
         public DateTime DateFin { get; private set; }
-
         public string Categorie { get; private set; }
-
         public string Nom { get; private set; }
-
         public string Observation { get; private set; }
+        
 
         public Produit(int codeProduit, DateTime dateEffet, DateTime dateFin, string categorie, string nom, string observation)
         {
@@ -29,6 +25,28 @@ namespace ApplicationJampay.Model.Entity
             Nom = nom;
             Observation = observation;
         }
-        
+
+        public bool Equals(Produit other)
+        {
+            return null != other && CodeProduit == other.CodeProduit;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Produit;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return this.CodeProduit.Equals(item.CodeProduit);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.CodeProduit.GetHashCode();
+        }
+
     }
 }
