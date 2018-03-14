2883221949742b9f81a6d11b356690701ca2dd21
Valentin REVERSAT <valxbox22@live.fr>
2018-03-14 21:12:19 +0100

    Refonte de la caisse (l'ajout se fait directement sur la fenêtre principale)

--- SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Caissier/UserControlFolder/UserControlCaisserViewModel.cs
+++ SolutionJampay/ApplicationJampay.ViewModel/ViewModel/Caissier/UserControlFolder/UserControlCaisserViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using ApplicationJampay.Model.Entity;
-using ApplicationJampay.Model.DAL.Usager;
 
 using ApplicationJampay.Model.Service.Dialog;
 using ApplicationJampay.ViewModel.Command;
@@ -8,13 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using ApplicationJampay.Model.Service.SmartCardReader;
-using ApplicationJampay.Model.DAL.Utilisateur;
+using ApplicationJampay.Model.DAL.Plat;
 
 namespace ApplicationJampay.ViewModel.ViewModel.Caissier.UserControlFolder
 {
@@ -29,8 +23,27 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier.UserControlFolder
         }
         #endregion
 
-
         #region Commands
+        private readonly RelayCommand _displayPlatCommand;
+        public ICommand DisplayPlatViewCommand => _displayPlatCommand;
+
+        private readonly RelayCommand _displayEntreeCommand;
+        public ICommand DisplayEntreeViewCommand => _displayEntreeCommand;
+
+        private readonly RelayCommand _displayDessertCommand;
+        public ICommand DisplayDessertViewCommand => _displayDessertCommand;
+
+        private readonly RelayCommand _displaySnackCommand;
+        public ICommand DisplaySnackViewCommand => _displaySnackCommand;
+
+        private readonly RelayCommand _validateCommand;
+        public ICommand ValidateCommand => _validateCommand;
+
+        private readonly RelayCommand _closeWindow;
+        public ICommand CloseWindow => _closeWindow;
+
+        private readonly RelayCommand _cardPayCommand;
+        public ICommand CardPayCommand => _cardPayCommand;
 
         private readonly RelayCommand _openAddPlatViewCommand;
         public ICommand OpenAddPlatViewCommand => _openAddPlatViewCommand;
@@ -39,14 +52,14 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier.UserControlFolder
         public ICommand DeleteSelectedChoosenPlat => _deleteSelectedChoosenPlat;
         #endregion
 
-        private readonly RelayCommand _cardPayCommand;
-        public ICommand CardPayCommand => _cardPayCommand;
 
-
-
+        private PlatBusiness _platBusiness;
 
         private ObservableCollection<PlatWithQuantité> _collectionChoosenPlat = new ObservableCollection<PlatWithQuantité>();
         public IEnumerable<PlatWithQuantité> CollectionChoosenPlat { get { return _collectionChoosenPlat; } }
+
+        private ObservableCollection<Plat> _coollectionOfSelectedCateg = new ObservableCollection<Plat>();
+        public IEnumerable<Plat> CollectionOfSelectedCateg { get { return _coollectionOfSelectedCateg; } }
 
         private float _prixTotal = 0;
 
@@ -56,8 +69,16 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier.UserControlFolder
             _deleteSelectedChoosenPlat = new RelayCommand(() => DeletePlat(), o => true);
             _cardPayCommand = new RelayCommand(() => PayCardReader(), o => true);
 
+            _platBusiness = new PlatBusiness();
 
-            Messenger.Default.Register<Plat>(this, (plat) => GetPlats(plat));
+            _displayPlatCommand = new RelayCommand(() => Plats(), o => true);
+            _displayEntreeCommand = new RelayCommand(() => Entrees(), o => true);
+            _displayDessertCommand = new RelayCommand(() => Desserts(), o => true);
+            _displaySnackCommand = new RelayCommand(() => Snacks(), o => true);
+
+            _validateCommand = new RelayCommand(() => GetPlats(_selectedPlat), o => true);
+
+            
         }
 
         private PlatWithQuantité _selectedChoosenPlat;
@@ -96,7 +117,7 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier.UserControlFolder
                 {
                     platWQ.Quantite++;
                     _prixTotal += plat.Prix;
-                    Prix = _prixTotal.ToString() + " €";
+                    Prix = (Math.Round(_prixTotal, 1)).ToString() + " €";
                     return;
                 }
             }
@@ -126,6 +147,110 @@ namespace ApplicationJampay.ViewModel.ViewModel.Caissier.UserControlFolder
         {
             DialogCaissier.ShowAjouterPlatWindow();
         }
+
+
+
+        private Plat _selectedPlat;
+        public Plat SelectedPlat
+        {
+            get
+            {
+                return _selectedPlat;
+            }
+            set
+            {
+                _selectedPlat = value;
+                OnPropertyChanged(nameof(SelectedPlat));
+            }
+        }
+        
+
+        private void Plats()
+        {
+            _coollectionOfSelectedCateg.Clear();
+
+            List<Plat> listPlats = new List<Plat>();
+            try
+            {
+                listPlats = _platBusiness.GetPlatbyCateg("Plat");
+
+                foreach (Plat plat in listPlats)
+                {
+                    _coollectionOfSelectedCateg.Add(plat);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogService.ShowErrorWindow(ex.Message);
+            }
+        }
+
+        private void Entrees()
+        {
+            _coollectionOfSelectedCateg.Clear();
+
+            List<Plat> listEntrees = new List<Plat>();
+
+            try
+            {
+                listEntrees = _platBusiness.GetPlatbyCateg("Entree");
+
+                foreach (Plat entree in listEntrees)
+                {
+                    _coollectionOfSelectedCateg.Add(entree);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogService.ShowErrorWindow(ex.Message);
+            }
+        }
+
+        private void Desserts()
+        {
+            _coollectionOfSelectedCateg.Clear();
+            List<Plat> listDesserts = new List<Plat>();
+
+            try
+            {
+                listDesserts = _platBusiness.GetPlatbyCateg("Dessert");
+
+                foreach (Plat dessert in listDesserts)
+                {
+                    _coollectionOfSelectedCateg.Add(dessert);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogService.ShowErrorWindow(ex.Message);
+            }
+        }
+
+        private void Snacks()
+        {
+            _coollectionOfSelectedCateg.Clear();
+
+            List<Plat> listSnacks = new List<Plat>();
+
+            try
+            {
+                listSnacks = _platBusiness.GetPlatbyCateg("Snack");
+
+                foreach (Plat snack in listSnacks)
+                {
+                    _coollectionOfSelectedCateg.Add(snack);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogService.ShowErrorWindow(ex.Message);
+            }
+
+        }
+
+
+
+
 
         private void PayCardReader()
         {
