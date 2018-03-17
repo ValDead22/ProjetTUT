80b3e57bb6fa9439cd5b2b8c117dbc4a0ed7d87a
Valentin REVERSAT <valxbox22@live.fr>
2018-03-17 23:45:00 +0100

    Fenetre pour gérer les produits

---
+++ SolutionJampay/ApplicationJampay/View/Gérant/PopUp/Produit/AjoutProduitView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationJampay.ViewModel.ViewModel.Gérant.PopUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ApplicationJampay.View.Gérant.PopUp.Produit
+{
+    /// <summary>
+    /// Logique d'interaction pour AjoutProduitView.xaml
+    /// </summary>
+    public partial class AjoutProduitView : Window
+    {
+        public AjoutProduitView()
+        {
+            DataContext = new AjoutProduitViewModel
+            {
+                Close = () => Close()
+        };
+            InitializeComponent();
+        }
+    }
+}
