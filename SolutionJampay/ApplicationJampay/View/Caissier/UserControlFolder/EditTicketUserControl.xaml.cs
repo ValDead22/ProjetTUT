34dc20a67213faac4881d4ec1198eaa204f16983
Valentin REVERSAT <valxbox22@live.fr>
2018-03-17 16:58:35 +0100

    Meilleut interface pour l'édition du ticket

--- SolutionJampay/ApplicationJampay/View/Caissier/UserControlFolder/EditTicketUserControl.xaml.cs
+++ SolutionJampay/ApplicationJampay/View/Caissier/UserControlFolder/EditTicketUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationJampay.ViewModel.ViewModel.Caissier.UserControlFolder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace ApplicationJampay.View.Caissier.UserControlFolder
     {
         public EditTicketUserControl()
         {
+            DataContext = new UserControlEditTicketViewModel();
             InitializeComponent();
         }
     }
